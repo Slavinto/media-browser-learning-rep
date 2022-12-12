06014a610b4cd90ce37462c1befb0ca474dd31e1
slavinto <slava3669@gmail.com>
2022-12-12 16:30:38 +0300

    added tab container and tab styling and tab list rendering in tab-container component

--- src/components/tab-container/tab-container.component.jsx
+++ src/components/tab-container/tab-container.component.jsx
@@ -1,10 +1,19 @@
 import Tab from "../tab/tab.component.jsx";
 import "./tab-container.styles.css";
+import { testData } from "../../data/data.js";
 
 const TabContainer = () => {
+  // todo:
+  // insert a react router link instead of tab.url
   return (
     <div className="tab-container">
-      <Tab></Tab>
+      {testData.map((tab) => {
+        return (
+          <div key={tab.id} className="tab">
+            <a href={tab.url}>**{tab.id}**</a>
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/components/tab/tab.component.jsx
+++ src/components/tab/tab.component.jsx
@@ -1,14 +1,10 @@
 import MediaPlayer from "../media-player/media-player.component.jsx";
 import FileBrowser from "../file-browser/file-browser.component.jsx";
 
-const Tab = () => {
+const Tab = ({ children }) => {
   return (
     <div className="tab">
-      <h3>Tab Component</h3>
-      <h4>
-        <FileBrowser></FileBrowser>
-        <MediaPlayer></MediaPlayer>
-      </h4>
+      <h4>{children}</h4>
     </div>
   );
 };
